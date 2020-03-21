8494dfb3bde3ea04d671f6b031a051b12728752a
robin-rademacher <mail@robin-rademacher.de>
2020-03-21 16:52:00 +0100

    Update

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@
 
 import sys
 
-from PyQt5.QtGui import QGuiApplication
-from PyQt5.QtQml import QQmlApplicationEngine
+from PySide2.QtGui import QGuiApplication
+from PySide2.QtQml import QQmlApplicationEngine
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
     engine.load("Main.qml")
-    
+
     engine.quit.connect(app.quit)
     sys.exit(app.exec_())
